2ecd5b3adb1515e26d97c64e02f9cec697eb3e57
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-20 23:29:49 +0000

    changes

--- eis_triangles.py
+++ eis_triangles.py
@@ -19,24 +19,20 @@ def main():
         x_number = int(x_string)
         y_number = int(y_string)
         z_number = int(z_string)
-        if x_number == y_number == z_number:
-            print(
-                "\nThe angles {0} {1} and {2} make an Equilateral triangle!".format(
-                    x_string, y_string, z_string
-                )
-            )
-        elif x_number == y_number or y_number == z_number or x_number == z_number:
-            print(
-                "\nThe angles {0} {1} and {2} make an Isosceles triangle!".format(
-                    x_string, y_string, z_string
-                )
-            )
+        if (
+            x_number + y_number + z_number == 180
+            and x_number > 0
+            and y_number > 0
+            and z_number > 0
+        ):
+            if x_number == y_number and y_number == z_number and z_number == x_number:
+                print("\nThese angles make an Equilateral triangle!")
+            elif x_number == y_number or y_number == z_number or x_number == z_number:
+                print("\nThese angles make an Isosceles triangle!")
+            else:
+                print("These angles make a Scalene triangle!")
         else:
-            print(
-                "\nThe angles {0} {1} and {2} make a Scalene triangle!".format(
-                    x_string, y_string, z_string
-                )
-            )
+            print("This is not a triangle")
     except ValueError:
         print("\nSorry This is invalid input")
     finally:
